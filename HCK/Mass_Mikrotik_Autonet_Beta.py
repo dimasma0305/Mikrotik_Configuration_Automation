9c21cdd71fb3efef66f37200d99d1f7be762d450
dimasma0305 <dimasmaulana0305@gmail.com>
2022-01-20 07:34:29 +0800

    sd

--- HCK/Mass_Mikrotik_Autonet_Beta.py
+++ HCK/Mass_Mikrotik_Autonet_Beta.py
@@ -3,17 +3,17 @@ import time
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-addr="192.168.122."
-hst=list(range(90,100))
+addr="192.168.122."#network id
+hst=list(range(90,100))#host id pool
 print(hst)
-usr="admin"
-pswd=""
+usr="admin"#username
+pswd=""#password
 
 for a in hst:
     try:
         ssh.connect(hostname=addr+str(a), username=usr, password=pswd, timeout=0.5)
         print(addr+str(a),"connected")
-        stdin, stdout, stderr = ssh.exec_command("/ip ro pr")
+        stdin, stdout, stderr = ssh.exec_command("/ip ro pr")#perintah ssh
         print(stdout.read().decode("ascii").strip("\n"))
     except:
         print(addr+str(a), "timeout")
